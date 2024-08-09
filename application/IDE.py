b7b0891fe503bb8484f157c4265db07cd2a332b7
GarciaAlegria <abnerivan2003@gmail.com>
2024-08-08 19:03:17 -0600

    cambios en la ide

--- application/IDE.py
+++ application/IDE.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 import subprocess
-from program.Driver import compilador
 import sys
+import os
+
+# Asegúrate de que el directorio raíz del proyecto esté en el PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'C:/Users/Personal/Documents/Universidad/8 Semestre/Compis2/SintaxAnalisis-Compiladores')))
+
+from program.Driver import compilador
 
 # Crear una instancia de Tkinter
 root = tk.Tk()
@@ -11,25 +16,31 @@ root = tk.Tk()
 root.title("Compiscript IDE")
 
 # Configurar el tamaño de la ventana
-root.geometry("600x400")
+root.geometry("600x500")
 
 # Crear una etiqueta
 label = ttk.Label(root, text="¡Escribe tu código Compiscript!")
-label.pack(pady=20)
+label.pack(pady=10)
 
 # Crear un campo de entrada de texto
-text_input = tk.Text(root, height=15, width=70)
-text_input.pack(pady=20)
+text_input = tk.Text(root, height=10, width=70)
+text_input.pack(pady=10)
+
+# Crear un área de texto para mostrar errores
+error_output = tk.Text(root, height=10, width=70, bg='light gray')
+error_output.pack(pady=10)
 
 # Crear una función para el botón
 def boton_click():
     codigo = text_input.get("1.0", tk.END)  # Obtener el texto del campo de entrada
+    error_output.delete("1.0", tk.END)  # Limpiar el área de texto de errores
     try:
-        # resultado = subprocess.check_output([sys.executable, "Driver.py", codigo], stderr=subprocess.STDOUT, text=True)
         resultado = compilador(codigo)
-        label.config(text=f"Compilación exitosa: {resultado}")
-    except subprocess.CalledProcessError as e:
-        label.config(text=f"Error de compilación: {e.output}")
+        label.config(text="Compilación exitosa")
+        error_output.insert(tk.END, f"Resultado: {resultado}")
+    except Exception as e:
+        label.config(text="Error de compilación")
+        error_output.insert(tk.END, str(e))
 
 # Crear un botón
 boton = ttk.Button(root, text="Compilar", command=boton_click)
